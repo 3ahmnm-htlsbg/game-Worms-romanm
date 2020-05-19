8aa2cba037c98f304f2f8c1b27e7995b253f562f
romanmoeseneder <roman.moeseneder03@htl-salzburg.ac.at>
2020-05-19 15:01:49 +0200

    code add

---
+++ Assets/Scrips/wormcontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour {
+    public KeyCode jump;
+    public KeyCode foward;
+    public Rigidbody z;
+    public Vector3 x;
+    public Vector3 y;
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (jump)) {
+            Debug.Log ("Jump taste wurde gedrückt");
+            z.AddForce (x);
+        }
+
+        if (Input.GetKeyDown (foward)) {
+            Debug.Log ("Forwärts taste wurde gedrückt");
+            z.AddForce (y);
+
+        }
+    }
+}
